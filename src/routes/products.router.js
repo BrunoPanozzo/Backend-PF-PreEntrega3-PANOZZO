e8423ee61c08cc61649f3add5fddab75c9b95a34
Administrador <Administrador@POLLUXC04>
2024-05-31 10:36:55 -0300

    mas cambios

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -4,6 +4,7 @@ const { validateNewProduct, validateUpdateProduct, validateProduct } = require('
 
 const ProducsController = require('../controllers/products.controller')
 const { PUBLIC, USER, ADMIN, SUPER_ADMIN } = require('../config/policies.constants')
+const { userIsAdmin } = require('../middlewares/auth.middleware')
 
 const withController = callback => {
     return (req, res) => {
@@ -18,11 +19,11 @@ class ProductRouter extends BaseRouter {
 
         this.get('/:pid', [USER], validateProduct, withController((controller, req, res) => controller.getProductById(req, res)))
 
-        this.post('/create', [ADMIN, SUPER_ADMIN], validateNewProduct, withController((controller, req, res) => controller.addProduct(req, res)))
+        this.post('/create', [ADMIN, SUPER_ADMIN], userIsAdmin, validateNewProduct, withController((controller, req, res) => controller.addProduct(req, res)))
 
-        this.put('/:pid', [ADMIN, SUPER_ADMIN], validateUpdateProduct, withController((controller, req, res) => controller.updateProduct(req, res)))
+        this.put('/:pid', [ADMIN, SUPER_ADMIN], userIsAdmin, validateUpdateProduct, withController((controller, req, res) => controller.updateProduct(req, res)))
 
-        this.delete('/:pid', [ADMIN, SUPER_ADMIN], validateProduct, withController((controller, req, res) => controller.deleteProduct(req, res)))
+        this.delete('/:pid', [ADMIN, SUPER_ADMIN], userIsAdmin, validateProduct, withController((controller, req, res) => controller.deleteProduct(req, res)))
 
     }
 }
